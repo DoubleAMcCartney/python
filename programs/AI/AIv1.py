1184b6d7d9cbeed112aeaa9229a7609e578b4732
DoubleAMcCartney <amm343@zips.uakron.edu>
2016-11-03 10:32:31 -0400

    Update AIv1.py
    AI class can now except any amount of stateVars, and return any amount of actions

--- programs/AI/AIv1.py
+++ programs/AI/AIv1.py
@@ -1,87 +1,183 @@
 import random, math
 
+from bisect    import insort
+from itertools import islice
+
 class AI():
-    def __init__(self):
-        self.deviation = [0.5, 0.2, 0.08, 0.03]
-        self.devMultiplier = 6.5
-        self.rewardThreshold = 20
+    def __init__(self, actions, stateVars):
+        self.maxData = 10000
+        self.rewardThreshold = 5
+        
         self.allSteps = []
         self.episodeSteps = []
-        self.minTries = 2
+        self.actions = actions
+        self.stateVars = stateVars
+        self.minTries = math.factorial(self.actions)
+        self.weight = [1] * self.stateVars
+        self.stdDev = [0] * self.stateVars
+        #self.maxSim = math.factorial(self.stateVars) * self.actions + 1
+        self.maxSim = 49
+        self.simSlope = 1 / self.maxSim
 
-    #find similar stats
-    def getSimStates(self, state, deviation):
+    #find statistacal significance of each state element
+    def findWeight(self):
+        states = len(self.allSteps)
+
+        if states < 2:
+            return
+        
+        #find mean action
+        actMean = 0
+        for i in range(states):
+            actMean += self.allSteps[i][(self.stateVars)]
+        actMean /= states
+
+        #find std def of actions
+        actStdDev = 0
+        for i in range(states):
+            actStdDev += math.pow((self.allSteps[i][(self.stateVars)]-actMean),2)
+        actStdDev = math.sqrt(actStdDev/(states-1))
+
+        if actStdDev == 0:
+            return
+
+        #find mean reward
+        rewMean = 0
+        for i in range(states):
+            rewMean += self.allSteps[i][(self.stateVars+1)]
+        rewMean /= states
+        
+        #find std dev of rewards
+        rewStdDev = 0
+        for i in range(states):
+            rewStdDev += math.pow((self.allSteps[i][(self.stateVars+1)]-rewMean),2)
+        rewStdDev = math.sqrt(rewStdDev/(states-1))
+
+        if rewStdDev == 0:
+            return
+
+        #find correlation between action and reward
+        rActRew = 0
+        for i in range(states):
+            rActRew += ((self.allSteps[i][self.stateVars]-actMean)/actStdDev)*((self.allSteps[i][self.stateVars+1]-rewMean)/rewStdDev)
+        rActRew /= states
+    
+        #find 2-dim correlations with states
+        rStateAct = [None] * self.stateVars
+        rStateRew = [None] * self.stateVars
+        for var in range(self.stateVars):
+            
+            #find mean of state element
+            mean = 0
+            for i in range(states):
+                mean += self.allSteps[i][var]
+            mean /= states
+
+            #find std dev of state element
+            self.stdDev[var] = 0
+            for i in range(states):
+                self.stdDev[var] += math.pow((self.allSteps[i][var]-mean),2)
+            self.stdDev[var] = math.sqrt(self.stdDev[var]/(states-1))
+
+            #find correlation of state element to action
+            r = 0
+            for i in range(states):
+                r += ((self.allSteps[i][var]-mean)/self.stdDev[var])*((self.allSteps[i][self.stateVars]-actMean)/actStdDev)
+            r /= states
+            rStateAct[var] = r
+
+            #find correlation of state element to reward
+            r = 0
+            for i in range(states):
+                r += ((self.allSteps[i][var]-mean)/self.stdDev[var])*((self.allSteps[i][self.stateVars+1]-rewMean)/rewStdDev)
+            r /= states
+            rStateRew[var] = r
+
+        # calculate correlation of each state variable and action pair to reward
+        self.weight = []
+        for i in range(self.stateVars):
+            self.weight.append(math.sqrt(math.fabs((math.pow(rStateRew[i],2)+math.pow(rActRew,2)-2*rStateAct[i]*rStateRew[i]*rActRew)-rStateAct[i])))
+
+        print(str(self.weight))
+
+    #return n closest states:
+    def closestStates(self, state, n):
         simStates = []
-        for states in range(len(self.allSteps)):
-            if (math.fabs(state[0] - self.allSteps[states][0]) < deviation[0] and
-                math.fabs(state[1] - self.allSteps[states][1]) < deviation[1] and
-                math.fabs(state[2] - self.allSteps[states][2]) < deviation[2] and
-                math.fabs(state[3] - self.allSteps[states][3]) < deviation[3] and
-                math.copysign(1.0,state[0])==math.copysign(1.0,self.allSteps[states][0]) and
-                math.copysign(1.0,state[1])==math.copysign(1.0,self.allSteps[states][1]) and
-                math.copysign(1.0,state[2])==math.copysign(1.0,self.allSteps[states][2]) and
-                math.copysign(1.0,state[3])==math.copysign(1.0,self.allSteps[states][3])):
-                simStates.append(self.allSteps[states])
-        else:
-            return simStates
+        steps = len(self.allSteps)
+        
+        diffs = []
+        for i in range(steps):
+            diff = 0
+            for j in range(self.stateVars):
+                diff += math.fabs((self.weight[j] * ((self.allSteps[i][j]-state[j])/self.stdDev[j])))
+            diffs.append(diff)
+
+        #find nth lowest diff
+        it   = iter(diffs)
+        mins = sorted(islice(it, n))
+        for el in it:
+            if el < mins[-1]:
+                insort(mins, el)
+                mins.pop()
+        maxDiff = mins[-1]
+
+        for i in range(steps):
+            if diffs[i] <= maxDiff:
+                simStates.append(self.allSteps[i])
+           
+        return simStates
+
 
     #detemins next action
     def getAction(self, state):
-        simStates = self.getSimStates(state, self.deviation)
+        steps = len(self.allSteps)
 
-        if len(simStates) < 10:
-            deviation = list(self.deviation)
-            for i in range(len(self.deviation)):
-                deviation[i] = self.devMultiplier*self.deviation[i]
-            simStates = self.getSimStates(state, deviation)
-        if len(simStates) < 10:
-            #print('Less than 10 sim states')
-            return random.randint(0,1)
-        else:
-            #ensure that each action has been tried at least self.minTries
-            act1 = simStates[0][4]
-            act2count = 0
-            for states in range(len(simStates)):
-                if simStates[states][4] != act1:
-                    act2count += 1
-            if act2count < self.minTries:
-                if act1 == 0:
-                    return 1
-                else:
-                    return 0
+        n = int((self.maxSim*((1/(self.maxSim*(1/self.simSlope)))*len(self.allSteps))/(math.sqrt((1+(1/(self.maxSim*(1/self.simSlope))*len(self.allSteps))**2)))))
+        if n == 0:
+            return random.randint(0,(self.actions-1))
+        simStates = self.closestStates(state, n)
+    
+        #ensure that each action has been tried at least self.minTries
+        for i in range(self.actions):
+            count = 0
+            for j in range((len(simStates))):
+                if simStates[j][self.stateVars] == i:
+                    count += 1
+            if count < self.minTries:
+                return i
 
         #calculate avg reward of each action for the sim states
-        state0 = []
-        state1 = []
-        state0Reward = 0
-        state1Reward = 0
-        lenState0 = 0
-        lenState1 = 0
-        for i in range(len(simStates)):
-            if simStates[i][4] == 0:
-                state0.append(simStates[i])
-                state0Reward += simStates[i][5]
-                lenState0 += 1
-            else:
-                state1.append(simStates[i])
-                state1Reward += simStates[i][5]
-                lenState1 += 1
-        state0Reward /= lenState0
-        state1Reward /= lenState1
+        statejReward = 0
+        avgRewards = [0]*self.actions
+        avgRew = 0
+        for j in range(self.actions):
+            lenStatej = 0
+            for i in range(len(simStates)):
+                if simStates[i][self.stateVars] == j:
+                    avgRewards[j] += simStates[i][self.stateVars+1]
+                    lenStatej += 1
+            avgRewards[j] /= lenStatej
+            avgRew += avgRewards[j]          
+        avgRew /= self.actions
 
-        #the higher the diff in reward, the less likely a random action will be taken
-        if (state1Reward >= state0Reward):
-            if random.randint(0,100)>((100/self.rewardThreshold)*state1Reward-state0Reward):
-                #print('Random 1')
-                return random.randint(0,1)
-            else:
-                return 1
-        else:
-            if random.randint(0,100)>((100/self.rewardThreshold)*state0Reward-state1Reward):
-                #print('Random 2')
-                return random.randint(0,1)
-            else:
-                return 0
+        if avgRew == 0:
+            return random.randint(0,1)
+
+        confidence = [0]*self.actions
+        for j in range(self.actions):
+            confidence[j] = 100*((avgRewards[j]-avgRew) / avgRew)
+            if confidence[j] == max(confidence):
+                if random.randint(0,100)>((100/self.rewardThreshold)*(confidence[j])):
+                    return random.randint(0,(self.actions-1))
+                else:
+                    return j    
+        return random.randint(0,(self.actions-1))
+       
+            
+    def deleteSteps(self):
+        while len(self.allSteps) > self.maxData:
+            del self.allSteps[random.randint(0,(len(self.allSteps)-1))]
+            
 
     #function called by environment     
     def getState(self, state, reward):
@@ -91,16 +187,17 @@ class AI():
         state.append(0) #add reward
         self.episodeSteps.append(state) #add state to list of states in episode
 
-        if reward == 1:
-            #add 1 to reward of each state in episode
-            #for any given state, the reward increases as the episode continues
-            for states in range(len(self.episodeSteps)):
-                self.episodeSteps[states][5] += reward
-        else:
+        if reward == 0:
             #add data from all states in episode when episode ends
             for states in range(len(self.episodeSteps)):
                 self.allSteps.append(self.episodeSteps[states])
             self.episodeSteps = []
-
+            self.findWeight()
+            if len(self.allSteps) > self.maxData:
+                self.deleteSteps()
+        else:
+            #add 1 to reward of each state in episode
+            #for any given state, the reward increases as the episode continues
+            for states in range(len(self.episodeSteps)):
+                self.episodeSteps[states][self.stateVars + 1] += reward
         return action
-
